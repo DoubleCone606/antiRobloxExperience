b22fc5f8abf4a4924308c523cfec08585feb1b39
DoubleCone606 <doublecone606@gmail.com>
2025-07-11 19:58:09 +0100

    Update begoneV3.js

--- scripts/begoneV3.js
+++ scripts/begoneV3.js
@@ -1,1 +1,3 @@
-
+(function(){
+  document.body.innerHTML = document.body.innerHTML.replace(/experience/g, "game").replace(/Experience/g, "Game").replace(/Experiences/g,"Games").replace(/experiences/g,"games").replace(/Charts/g,"Games").replace(/charts/g,"games"); 
+})();
